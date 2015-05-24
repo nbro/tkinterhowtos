0f82fb9704e154b9f96fc63e0b85ebb6bebcf020
nbro <dossan@usi.ch>
2015-05-25 01:21:39 +0200

    Console widget updated...

--- personal/widgets/Console.py
+++ personal/widgets/Console.py
@@ -1,5 +1,5 @@
 """
-Simple console that accepts bash commands
+Simple console that executes bash commands
 
 Author: Nelson Brochado
 Version: 0.0.1
@@ -10,36 +10,32 @@ import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 import threading
 from subprocess import Popen, PIPE
-import subprocess
 
 
 class Console(tk.Frame):
 
     """Simple console that can execute bash commands"""
 
-    MESSAGE = """+===========================================================================+
-|Insert a command in the entry field and press 'Execute' to run the command.|
-|If you need to stop the command before it terminates, click 'Stop'.        |
-|To clear the console output, click button 'Clear'.                         |
-+===========================================================================+\n\n"""
-
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
 
-        self.text = ScrolledText(self, state="normal", bg="black", fg="#08c614",
-                                 insertbackground="#08c614", selectbackground="#f01c1c")
-
-        self.text.insert("end", Console.MESSAGE)
-        self.text.config(state="disabled")
+        self.text_options = {"state": "disabled",
+                             "bg": "black",
+                             "fg": "#08c614",
+                             "insertbackground": "#08c614",
+                             "selectbackground": "#f01c1c"}
+        
+        self.text = ScrolledText(self, **self.text_options)
 
         # It seems not to work when Text is disabled...
         # self.text.bind("<<Modified>>", lambda: self.text.frame.see(tk.END))
-        
+
         self.text.pack(expand=True, fill="both")
 
-        # command that will be executed when "Execute" is pressed
-        self.command = ""  # bash command
-        self.popen = None  # will hold a reference to a Popen object
+        # bash command, for example 'ping localhost' or 'pwd'
+        # that will be executed when "Execute" is pressed
+        self.command = ""  
+        self.popen = None     # will hold a reference to a Popen object
         self.running = False  # True if the process is running
 
         self.bottom = tk.Frame(self)
@@ -47,78 +43,88 @@ class Console(tk.Frame):
         self.prompt = tk.Label(self.bottom, text="Enter the command: ")
         self.prompt.pack(side="left", fill="x")
         self.entry = tk.Entry(self.bottom)
-        self.entry.bind("<Return>", self._start)
+        self.entry.bind("<Return>", self.start_thread)
+        self.entry.bind("<Command-a>", lambda e: self.entry.select_range(0, "end"))
+        self.entry.bind("<Command-c>", self.clear)
         self.entry.focus()
         self.entry.pack(side="left", fill="x", expand=True)
 
-        self.opts = {"width": 6}
-        self.executer = tk.Button(self.bottom, text="Execute",
-                                  command=self._start, **self.opts)
+        self.executer = tk.Button(self.bottom, text="Execute", command=self.start_thread)
         self.executer.pack(side="left", padx=5, pady=2)
-        self.clearer = tk.Button(self.bottom, text="Clear",
-                                 command=self._clear, **self.opts)
+        self.clearer = tk.Button(self.bottom, text="Clear", command=self.clear)
         self.clearer.pack(side="left", padx=5, pady=2)
-        self.stopper = tk.Button(self.bottom, text="Stop",
-                                 command=self._stop, **self.opts)
+        self.stopper = tk.Button(self.bottom, text="Stop", command=self.stop)
         self.stopper.pack(side="left", padx=5, pady=2)
 
         self.bottom.pack(side="bottom", fill="both")
 
-        
-
-    def _clear(self):
-        self._stop()
+    def clear_text(self):
+        """Clears the Text widget"""
         self.text.config(state="normal")
         self.text.delete(1.0, "end-1c")
-        self.text.insert("end", Console.MESSAGE)
         self.text.config(state="disabled")
 
-    def _start(self, event=None):
-        self._stop()
+    def clear_entry(self):
+        """Clears the Entry command widget"""
+        self.entry.delete(0, "end")
+        
+    def clear(self, event=None):
+        """Does not stop an eventual running process,
+        but just clears the Text and Entry widgets."""
+        self.clear_entry()
+        self.clear_text()
+
+    def show(self, message):
+        """Inserts message into the Text wiget"""
+        self.text.config(state="normal")
+        self.text.insert("end", message)
+        self.text.see("end")
+        self.text.config(state="disabled")
+
+    def start_thread(self, event=None):
+        """Starts a new thread and calls process"""
+        self.stop()
         self.running = True
         self.command = self.entry.get()
-        threading.Thread(target=self._start_process).start()
+        # self.process is called by the Thread's run method
+        threading.Thread(target=self.process).start()
 
-    def _start_process(self):
+    def process(self):
+        """Runs in an infinite loop until self.running is False""" 
         while self.running:
-            self._execute(self.command)
+            self.execute()
 
-    def _stop(self):
+    def stop(self):
+        """Stops an eventual running process"""
         if self.popen:
             try:
                 self.popen.kill()
             except ProcessLookupError:
-                pass
+                pass 
         self.running = False
 
-    def _execute(self, command):
-        self.text.config(state="normal")
+    def execute(self):
+        """Keeps inserting line by line into self.text
+        the output of the execution of self.command"""
         try:
-            # self.popen will be a Popen object
-            self.popen = Popen(command.split(), stdout=PIPE, bufsize=1)
+            # self.popen is a Popen object
+            self.popen = Popen(self.command.split(), stdout=PIPE, bufsize=1)
             lines_iterator = iter(self.popen.stdout.readline, b"")
 
             # poll() return None if the process has not terminated
             # otherwise poll() returns the process's exit code
             while self.popen.poll() is None:
                 for line in lines_iterator:
-                    dline = line.decode("utf-8")
-                    self.text.insert("end", dline)
-                    self.text.see("end")
-
-            self.text.insert("end", command  + " terminated.\n\n")
-
+                    self.show(line.decode("utf-8"))
+            self.show("Process " + self.command  + " terminated.\n\n")
+            
         except FileNotFoundError:
-            self.text.insert("end", "Unknown command: " + command + "\n\n")
-        except PermissionError:
-            self.text.insert("end", "Empty command: " + command + "\n\n")
-        except Exception:
-            pass
-
-        self.text.see("end")
-        self.text.config(state="disabled")
-        self._stop()
-
+            self.show("Unknown command: " + self.command + "\n\n")                               
+        except IndexError:
+            self.show("No command entered\n\n")
+            
+        self.stop()
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
