43273bca06fa78c94fc926ece849511c4c6ea2f9
nelson-brochado <nelson.brochado@outlook.com>
2017-09-02 11:58:19 +0200

    Fixed a little thing

--- widgets/Text/highlight_current_line.py
+++ widgets/Text/highlight_current_line.py
@@ -13,6 +13,7 @@ An example on how to highlight the current line in a Text widget while writing.
 # References
 
 - https://stackoverflow.com/a/9720858/3924118
+- http://effbot.org/tkinterbook/text.htm#Tkinter.Text.tag_add-method
 """
 
 from tkinter import Tk, Frame, Text
@@ -25,16 +26,31 @@ class Example(Frame):
         self.text = Text(self)
         self.text.pack(side="top", fill="both", expand=True)
         self.text.tag_configure("current", background="#e9e9e9")
-
         self.highlight_current_line()
 
     def highlight_current_line(self, interval=50):
         self.text.tag_remove("current", 1.0, "end")
+
+        # insert is a named index and corresponds to the insertion cursor.
+        #
+        # Expressions can be used to modify any kind of index. Expressions are
+        # formed by taking the string representation of an index, like "insert",
+        # and appending one or more modifiers.
+        #
+        # In this case, we append the "linestart" modifier to "insert" as the
+        # first index, and "lineend" and "+1c" to "insert" as last index.
+        #
+        # linestart moves the index to the first position on the line.
+        #
+        # lineend is the index to the last position on the line (the newline).
+        #
+        # +1c is an abbreviation of "+ 1 chars", and means to move the index
+        # forward, which is moved over newlines, but not beyond the END index.
         self.text.tag_add("current", "insert linestart", "insert lineend+1c")
         self.after(interval, self.highlight_current_line)
 
 
 if __name__ == "__main__":
     root = Tk()
-    Example(root).pack()
+    Example(root).pack(fill="both", expand=True)
     root.mainloop()
